2e9d57b0d572f6248888ba1e2e3fa05c0acabdb5
Igor Gurgel <106691518+Iguilti@users.noreply.github.com>
2023-09-29 02:46:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+// const errorTypes = [
+//     'valueMissing',
+//     'typeMismatch',
+//     'tooLong'
+// ]
+
+// const errorMessages = {
+//     name: {
+//         valueMissing: "Este campo não pode estar vazio.",
+//         tooLong: "Número máximo de caracteres excedido."
+//     },
+//     email: {
+//         valueMissing: "Este campo não pode estar vazio.",
+//         typeMismatch: "Preencha um email válido.",
+//         tooLong: "Número máximo de caracteres excedido."
+//     },
+//     subject: {
+//         valueMissing: "Este campo não pode estar vazio.",
+//         tooLong: "Número máximo de caracteres excedido."
+//     },
+//     message: {
+//         valueMissing: "Este campo não pode estar vazio.",
+//         tooLong: "Número máximo de caracteres excedido."
+//     }
+// }
+
